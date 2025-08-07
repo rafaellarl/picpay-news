023966d36fa2e9e13175d2fb86e20b2a8cf600ff
rafaellarl <rafaellar.dev@gmail.com>
2025-08-06 21:30:34 -0300

    config firebase ios

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import RootStack from './routes';
+import firebase from '@react-native-firebase/app';
 
 function App() {
+  console.log('firebase: ', firebase.app());
   return (
       <NavigationContainer>
         <SafeAreaView style={{ flex: 1 }}>
