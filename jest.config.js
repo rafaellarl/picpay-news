df9f6260b709bfdb21e0abf55a7c087110b83878
rafaellarl <rafaellar.dev@gmail.com>
2025-08-08 05:20:17 -0300

    config tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*))',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text-summary'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
 };
