303a8deee57e09db7f67d30832356b6de97d70cd
rafaellarl <rafaellar.dev@gmail.com>
2025-08-09 16:15:47 -0300

    add test for CustomHeader component

--- src/components/CustomHeader/CustomHeader.index.tsx
+++ src/components/CustomHeader/CustomHeader.index.tsx
@@ -5,17 +5,23 @@ import {View, Text, TouchableOpacity, Image} from 'react-native';
 import styles from './CustomHeader.styles';
 
 interface ICustomHeaderProps {
-  title: string;
+  title?: string;
   showBackButton?: boolean;
 }
 
-const CustomHeader = ({title, showBackButton = true}: ICustomHeaderProps) => {
+const CustomHeader = ({
+  title = '',
+  showBackButton = true,
+}: ICustomHeaderProps) => {
   const {goBack} = useNavigation();
 
   return (
     <View style={styles.container}>
       {showBackButton && (
-        <TouchableOpacity onPress={() => goBack()} style={styles.backButton}>
+        <TouchableOpacity
+          onPress={goBack}
+          style={styles.backButton}
+          testID="test-id-header-back-button">
           <Image
             source={require('../../assets/images/arrow-left-white.png')}
             style={styles.backIcon}
@@ -23,7 +29,9 @@ const CustomHeader = ({title, showBackButton = true}: ICustomHeaderProps) => {
         </TouchableOpacity>
       )}
 
-      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.title} testID="test-id-header-title">
+        {title}
+      </Text>
       <View style={styles.rightPlaceholder} />
     </View>
   );
---
+++ src/components/CustomHeader/CustomHeader.spec.tsx
@@ -0,0 +1,63 @@
+import {fireEvent, render, screen} from '@testing-library/react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import CustomHeader from './CustomHeader.index';
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+}));
+
+const mockedUseNavigation = useNavigation as jest.Mock;
+const propsMock = {
+  title: 'Título header',
+  showBackButton: false,
+};
+
+describe('CustomHeader', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockedUseNavigation.mockReturnValue({
+      goBack: jest.fn(),
+    });
+  });
+
+  describe('Render correctly', () => {
+    it('Should render whit all elements', () => {
+      const {getByText, getByTestId} = render(
+        <CustomHeader title={propsMock.title} showBackButton />,
+      );
+
+      getByText('Título header');
+      getByTestId('test-id-header-back-button');
+    });
+
+    it('Should render whitout title', () => {
+      const {getByTestId} = render(<CustomHeader showBackButton />);
+
+      getByTestId('test-id-header-back-button');
+      expect(getByTestId('test-id-header-title').props.title).toBeFalsy();
+    });
+
+    it('Should render whitout backbutton', () => {
+      const {getByText, queryByTestId} = render(
+        <CustomHeader
+          title={propsMock.title}
+          showBackButton={propsMock.showBackButton}
+        />,
+      );
+
+      getByText('Título header');
+      expect(queryByTestId('test-id-header-back-button')).toBeNull();
+    });
+  });
+
+  describe('Handle back button actions', () => {
+    it('Should direct the user to the previous screen', () => {
+      const {goBack} = mockedUseNavigation();
+      const {getByTestId} = render(<CustomHeader showBackButton />);
+
+      fireEvent.press(getByTestId('test-id-header-back-button'));
+      expect(goBack).toHaveBeenCalledTimes(1);
+    });
+  });
+});
